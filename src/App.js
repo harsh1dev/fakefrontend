6f4948b83e25dfe5f313a6e8f55ab141a2296f6d
harsh1dev <harshkumar1.dev@gmail.com>
2024-10-23 00:35:59 +0530

    post endpoint integration with UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
+import { useState } from 'react';
+import { Input } from 'antd';
+import { Button, Flex } from 'antd';
+
+const { TextArea } = Input;
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className='main'>
+      <Home />
     </div>
   );
+}
+
+
+function Home() {
+  const [inputValue, seInputValue] = useState('');
+
+  const handleSubit = async () => {
+    const response = await fetch('http://localhost:8000/data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: inputValue }),
+    });
+
+    const result = await response.json();
+    console.log('Success:', result);
+
+
+  };
+
+  return (
+    <div className='home'>
+      <TextArea rows={20}
+        onChange={(e) => seInputValue(e.target.value)}
+        className='textarea-input'
+      />
+      <Flex gap="small" wrap>
+        <Button type="primary" onClick={handleSubit}>Submit</Button>
+      </Flex>
+    </div>
+  )
 }
 
 export default App;
